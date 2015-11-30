c6dfac1b20590d735f693997384d171822fd4231
kevin robert <kevin.j.robert@gmail.com>
2015-11-30 11:01:18 +0100

    score recorded

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,15 @@ class GamesController  < ApplicationController
     @sequence = make_seq(1, 25)
     @game = current_user.games.create
   end
+
+  def index
+    @games = current_user.games
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    score = params[:game][:score].to_i
+    @game.update(score: score)
+    redirect_to games_path
+  end
 end
